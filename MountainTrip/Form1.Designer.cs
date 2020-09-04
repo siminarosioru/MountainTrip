058c07ecfe70cadad96a72d2de978719785d49f0
siminarosioru <simina.rosioru@gmail.com>
2020-09-04 15:32:45 +0200

    Add project files.

---
+++ MountainTrip/Form1.Designer.cs
@@ -0,0 +1,313 @@
+﻿namespace MountainTrip
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.checkBoxCort = new System.Windows.Forms.CheckBox();
+            this.linkCort = new System.Windows.Forms.LinkLabel();
+            this.linkSacDeDormit = new System.Windows.Forms.LinkLabel();
+            this.checkBoxIzopren = new System.Windows.Forms.CheckBox();
+            this.linkIzopren = new System.Windows.Forms.LinkLabel();
+            this.butonEnter = new System.Windows.Forms.Button();
+            this.checkBoxImbracaminte = new System.Windows.Forms.CheckBox();
+            this.linkImbracaminte = new System.Windows.Forms.LinkLabel();
+            this.checkBoxBocanci = new System.Windows.Forms.CheckBox();
+            this.checkBoxBeteDrumetie = new System.Windows.Forms.CheckBox();
+            this.linkBeteDrumetie = new System.Windows.Forms.LinkLabel();
+            this.checkBoxSacDeDormit = new System.Windows.Forms.CheckBox();
+            this.pictureBoxBocanci = new System.Windows.Forms.PictureBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBoxBeteDrumetie = new System.Windows.Forms.PictureBox();
+            this.pictureBoxImbracaminte = new System.Windows.Forms.PictureBox();
+            this.pictureBoxIzopren = new System.Windows.Forms.PictureBox();
+            this.pictureBoxSacDeDormit = new System.Windows.Forms.PictureBox();
+            this.linkBocanciDeMunte = new System.Windows.Forms.LinkLabel();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxBocanci)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxBeteDrumetie)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxImbracaminte)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxIzopren)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxSacDeDormit)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // checkBoxCort
+            // 
+            this.checkBoxCort.AutoSize = true;
+            this.checkBoxCort.Location = new System.Drawing.Point(32, 23);
+            this.checkBoxCort.Name = "checkBoxCort";
+            this.checkBoxCort.Size = new System.Drawing.Size(65, 24);
+            this.checkBoxCort.TabIndex = 1;
+            this.checkBoxCort.Text = "Cort";
+            this.checkBoxCort.UseVisualStyleBackColor = true;
+            // 
+            // linkCort
+            // 
+            this.linkCort.AutoSize = true;
+            this.linkCort.Location = new System.Drawing.Point(209, 24);
+            this.linkCort.Name = "linkCort";
+            this.linkCort.Size = new System.Drawing.Size(62, 20);
+            this.linkCort.TabIndex = 3;
+            this.linkCort.TabStop = true;
+            this.linkCort.Text = "linkCort";
+            this.linkCort.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.linkCort_LinkClicked);
+            // 
+            // linkSacDeDormit
+            // 
+            this.linkSacDeDormit.AutoSize = true;
+            this.linkSacDeDormit.Location = new System.Drawing.Point(277, 89);
+            this.linkSacDeDormit.Name = "linkSacDeDormit";
+            this.linkSacDeDormit.Size = new System.Drawing.Size(128, 20);
+            this.linkSacDeDormit.TabIndex = 6;
+            this.linkSacDeDormit.TabStop = true;
+            this.linkSacDeDormit.Text = "linkSacDeDormit";
+            this.linkSacDeDormit.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.linkSacDeDormit_LinkClicked);
+            // 
+            // checkBoxIzopren
+            // 
+            this.checkBoxIzopren.AutoSize = true;
+            this.checkBoxIzopren.Location = new System.Drawing.Point(32, 151);
+            this.checkBoxIzopren.Name = "checkBoxIzopren";
+            this.checkBoxIzopren.Size = new System.Drawing.Size(89, 24);
+            this.checkBoxIzopren.TabIndex = 7;
+            this.checkBoxIzopren.Text = "Izopren";
+            this.checkBoxIzopren.UseVisualStyleBackColor = true;
+            // 
+            // linkIzopren
+            // 
+            this.linkIzopren.AutoSize = true;
+            this.linkIzopren.Location = new System.Drawing.Point(233, 155);
+            this.linkIzopren.Name = "linkIzopren";
+            this.linkIzopren.Size = new System.Drawing.Size(86, 20);
+            this.linkIzopren.TabIndex = 9;
+            this.linkIzopren.TabStop = true;
+            this.linkIzopren.Text = "linkIzopren";
+            this.linkIzopren.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.linkIzopren_LinkClicked);
+            // 
+            // butonEnter
+            // 
+            this.butonEnter.Location = new System.Drawing.Point(583, 367);
+            this.butonEnter.Name = "butonEnter";
+            this.butonEnter.Size = new System.Drawing.Size(141, 71);
+            this.butonEnter.TabIndex = 10;
+            this.butonEnter.Text = "Enter";
+            this.butonEnter.UseVisualStyleBackColor = true;
+            this.butonEnter.Click += new System.EventHandler(this.butonEnter_Click);
+            // 
+            // checkBoxImbracaminte
+            // 
+            this.checkBoxImbracaminte.AutoSize = true;
+            this.checkBoxImbracaminte.Location = new System.Drawing.Point(32, 217);
+            this.checkBoxImbracaminte.Name = "checkBoxImbracaminte";
+            this.checkBoxImbracaminte.Size = new System.Drawing.Size(132, 24);
+            this.checkBoxImbracaminte.TabIndex = 11;
+            this.checkBoxImbracaminte.Text = "Imbracaminte";
+            this.checkBoxImbracaminte.UseVisualStyleBackColor = true;
+            // 
+            // linkImbracaminte
+            // 
+            this.linkImbracaminte.AutoSize = true;
+            this.linkImbracaminte.Location = new System.Drawing.Point(239, 221);
+            this.linkImbracaminte.Name = "linkImbracaminte";
+            this.linkImbracaminte.Size = new System.Drawing.Size(129, 20);
+            this.linkImbracaminte.TabIndex = 13;
+            this.linkImbracaminte.TabStop = true;
+            this.linkImbracaminte.Text = "linkImbracaminte";
+            this.linkImbracaminte.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.linkImbracaminte_LinkClicked);
+            // 
+            // checkBoxBocanci
+            // 
+            this.checkBoxBocanci.AutoSize = true;
+            this.checkBoxBocanci.Location = new System.Drawing.Point(32, 270);
+            this.checkBoxBocanci.Name = "checkBoxBocanci";
+            this.checkBoxBocanci.Size = new System.Drawing.Size(163, 24);
+            this.checkBoxBocanci.TabIndex = 14;
+            this.checkBoxBocanci.Text = "Bocanci de munte";
+            this.checkBoxBocanci.UseVisualStyleBackColor = true;
+            // 
+            // checkBoxBeteDrumetie
+            // 
+            this.checkBoxBeteDrumetie.AutoSize = true;
+            this.checkBoxBeteDrumetie.Location = new System.Drawing.Point(32, 345);
+            this.checkBoxBeteDrumetie.Name = "checkBoxBeteDrumetie";
+            this.checkBoxBeteDrumetie.Size = new System.Drawing.Size(135, 24);
+            this.checkBoxBeteDrumetie.TabIndex = 17;
+            this.checkBoxBeteDrumetie.Text = "Bete drumetie";
+            this.checkBoxBeteDrumetie.UseVisualStyleBackColor = true;
+            // 
+            // linkBeteDrumetie
+            // 
+            this.linkBeteDrumetie.AutoSize = true;
+            this.linkBeteDrumetie.Location = new System.Drawing.Point(279, 349);
+            this.linkBeteDrumetie.Name = "linkBeteDrumetie";
+            this.linkBeteDrumetie.Size = new System.Drawing.Size(131, 20);
+            this.linkBeteDrumetie.TabIndex = 19;
+            this.linkBeteDrumetie.TabStop = true;
+            this.linkBeteDrumetie.Text = "linkBeteDrumetie";
+            this.linkBeteDrumetie.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.linkBeteDrumetie_LinkClicked);
+            // 
+            // checkBoxSacDeDormit
+            // 
+            this.checkBoxSacDeDormit.AutoSize = true;
+            this.checkBoxSacDeDormit.Location = new System.Drawing.Point(32, 85);
+            this.checkBoxSacDeDormit.Name = "checkBoxSacDeDormit";
+            this.checkBoxSacDeDormit.Size = new System.Drawing.Size(133, 24);
+            this.checkBoxSacDeDormit.TabIndex = 21;
+            this.checkBoxSacDeDormit.Text = "Sac de dormit";
+            this.checkBoxSacDeDormit.UseVisualStyleBackColor = true;
+            // 
+            // pictureBoxBocanci
+            // 
+            this.pictureBoxBocanci.Image = global::MountainTrip.Properties.Resources.bocanci_de_munte1;
+            this.pictureBoxBocanci.Location = new System.Drawing.Point(201, 270);
+            this.pictureBoxBocanci.Name = "pictureBoxBocanci";
+            this.pictureBoxBocanci.Size = new System.Drawing.Size(100, 50);
+            this.pictureBoxBocanci.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBoxBocanci.TabIndex = 22;
+            this.pictureBoxBocanci.TabStop = false;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::MountainTrip.Properties.Resources.cort1;
+            this.pictureBox1.Location = new System.Drawing.Point(103, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(100, 50);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 20;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pictureBoxBeteDrumetie
+            // 
+            this.pictureBoxBeteDrumetie.Image = global::MountainTrip.Properties.Resources.bete_drumetie;
+            this.pictureBoxBeteDrumetie.Location = new System.Drawing.Point(173, 335);
+            this.pictureBoxBeteDrumetie.Name = "pictureBoxBeteDrumetie";
+            this.pictureBoxBeteDrumetie.Size = new System.Drawing.Size(100, 50);
+            this.pictureBoxBeteDrumetie.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBoxBeteDrumetie.TabIndex = 18;
+            this.pictureBoxBeteDrumetie.TabStop = false;
+            // 
+            // pictureBoxImbracaminte
+            // 
+            this.pictureBoxImbracaminte.Image = global::MountainTrip.Properties.Resources.imbracaminte;
+            this.pictureBoxImbracaminte.Location = new System.Drawing.Point(170, 217);
+            this.pictureBoxImbracaminte.Name = "pictureBoxImbracaminte";
+            this.pictureBoxImbracaminte.Size = new System.Drawing.Size(63, 37);
+            this.pictureBoxImbracaminte.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBoxImbracaminte.TabIndex = 12;
+            this.pictureBoxImbracaminte.TabStop = false;
+            // 
+            // pictureBoxIzopren
+            // 
+            this.pictureBoxIzopren.Image = global::MountainTrip.Properties.Resources.izopren;
+            this.pictureBoxIzopren.Location = new System.Drawing.Point(127, 151);
+            this.pictureBoxIzopren.Name = "pictureBoxIzopren";
+            this.pictureBoxIzopren.Size = new System.Drawing.Size(100, 50);
+            this.pictureBoxIzopren.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBoxIzopren.TabIndex = 8;
+            this.pictureBoxIzopren.TabStop = false;
+            // 
+            // pictureBoxSacDeDormit
+            // 
+            this.pictureBoxSacDeDormit.Image = global::MountainTrip.Properties.Resources.sac_de_dormit_captusit_cu_fleece_carp;
+            this.pictureBoxSacDeDormit.Location = new System.Drawing.Point(171, 85);
+            this.pictureBoxSacDeDormit.Name = "pictureBoxSacDeDormit";
+            this.pictureBoxSacDeDormit.Size = new System.Drawing.Size(100, 50);
+            this.pictureBoxSacDeDormit.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBoxSacDeDormit.TabIndex = 5;
+            this.pictureBoxSacDeDormit.TabStop = false;
+            // 
+            // linkBocanciDeMunte
+            // 
+            this.linkBocanciDeMunte.AutoSize = true;
+            this.linkBocanciDeMunte.Location = new System.Drawing.Point(307, 270);
+            this.linkBocanciDeMunte.Name = "linkBocanciDeMunte";
+            this.linkBocanciDeMunte.Size = new System.Drawing.Size(155, 20);
+            this.linkBocanciDeMunte.TabIndex = 23;
+            this.linkBocanciDeMunte.TabStop = true;
+            this.linkBocanciDeMunte.Text = "linkBocanciDeMunte";
+            this.linkBocanciDeMunte.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.linkBocanciDeMunte_LinkClicked);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.linkBocanciDeMunte);
+            this.Controls.Add(this.pictureBoxBocanci);
+            this.Controls.Add(this.checkBoxSacDeDormit);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.linkBeteDrumetie);
+            this.Controls.Add(this.pictureBoxBeteDrumetie);
+            this.Controls.Add(this.checkBoxBeteDrumetie);
+            this.Controls.Add(this.checkBoxBocanci);
+            this.Controls.Add(this.linkImbracaminte);
+            this.Controls.Add(this.pictureBoxImbracaminte);
+            this.Controls.Add(this.checkBoxImbracaminte);
+            this.Controls.Add(this.butonEnter);
+            this.Controls.Add(this.linkIzopren);
+            this.Controls.Add(this.pictureBoxIzopren);
+            this.Controls.Add(this.checkBoxIzopren);
+            this.Controls.Add(this.linkSacDeDormit);
+            this.Controls.Add(this.pictureBoxSacDeDormit);
+            this.Controls.Add(this.linkCort);
+            this.Controls.Add(this.checkBoxCort);
+            this.Name = "Form1";
+            this.Text = "Mountain trip list";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxBocanci)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxBeteDrumetie)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxImbracaminte)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxIzopren)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxSacDeDormit)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.CheckBox checkBoxCort;
+        private System.Windows.Forms.LinkLabel linkCort;
+        private System.Windows.Forms.PictureBox pictureBoxSacDeDormit;
+        private System.Windows.Forms.LinkLabel linkSacDeDormit;
+        private System.Windows.Forms.CheckBox checkBoxIzopren;
+        private System.Windows.Forms.PictureBox pictureBoxIzopren;
+        private System.Windows.Forms.LinkLabel linkIzopren;
+        private System.Windows.Forms.Button butonEnter;
+        private System.Windows.Forms.CheckBox checkBoxImbracaminte;
+        private System.Windows.Forms.PictureBox pictureBoxImbracaminte;
+        private System.Windows.Forms.LinkLabel linkImbracaminte;
+        private System.Windows.Forms.CheckBox checkBoxBocanci;
+        private System.Windows.Forms.CheckBox checkBoxBeteDrumetie;
+        private System.Windows.Forms.PictureBox pictureBoxBeteDrumetie;
+        private System.Windows.Forms.LinkLabel linkBeteDrumetie;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.CheckBox checkBoxSacDeDormit;
+        private System.Windows.Forms.PictureBox pictureBoxBocanci;
+        private System.Windows.Forms.LinkLabel linkBocanciDeMunte;
+    }
+}
+
---
+++ MountainTrip/Form1.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MountainTrip
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void butonEnter_Click(object sender, EventArgs e)
+        {
+            if (checkBoxCort.Checked)
+                MessageBox.Show("you checked the tent", "Output");
+            else if (checkBoxSacDeDormit.Checked)
+                MessageBox.Show("you checked the sleeping bag", "Output");
+            else if (checkBoxIzopren.Checked)
+                MessageBox.Show("you checked the isoprene mattress", "Output");
+            else if (checkBoxImbracaminte.Checked)
+                MessageBox.Show("you checked the hiking clothes", "Output");
+            else if (checkBoxBocanci.Checked)
+                MessageBox.Show("you checked the hiking boots", "Output");
+            else if (checkBoxBeteDrumetie.Checked)
+                MessageBox.Show("you checked the hiking sticks");
+        }
+
+        private void linkCort_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            System.Diagnostics.Process.Start("https://www.google.com/search?q=decathlon+cort&tbm=isch&ved=2ahUKEwiz3fvji8_rAhXO16QKHcvlDjoQ2-cCegQIABAA&oq=decathlon+cort&gs_lcp=CgNpbWcQAzICCAAyAggAMgIIADICCAAyAggAMgQIABAeMgQIABAeMgQIABAeMgQIABAeMgYIABAIEB5QmMgQWKXOEGCu0xBoAHAAeACAAbQBiAH2BJIBAzAuNJgBAKABAaoBC2d3cy13aXotaW1nwAEB&sclient=img&ei=EfxRX_OQEs6vkwXLy7vQAw&bih=578&biw=1280#imgrc=LRKjKjY6ev_D_M");
+        }
+
+        private void linkSacDeDormit_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            System.Diagnostics.Process.Start("https://www.google.com/search?q=decathlon+sac+de+dormit&sxsrf=ALeKk0011Zjagy64gAfINpQ9peSrTNF3IA:1599208451107&source=lnms&tbm=isch&sa=X&ved=2ahUKEwjdq5ndi8_rAhXIQUEAHflNBVEQ_AUoAXoECAwQAw&biw=1280&bih=578#imgrc=YWp7XCWkK0cV1M");
+        }
+
+        private void linkIzopren_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            System.Diagnostics.Process.Start("https://www.google.com/search?q=saltea+izopren+decathlon&tbm=isch&ved=2ahUKEwib8d3Aks_rAhXSAewKHezZCYIQ2-cCegQIABAA&oq=decathlon+izopre&gs_lcp=CgNpbWcQARgBMgQIIxAnMgYIABAIEB5QtJwDWLScA2DhrgNoAHAAeACAAW-IAW-SAQMwLjGYAQCgAQGqAQtnd3Mtd2l6LWltZ8ABAQ&sclient=img&ei=HgNSX5vqG9KDsAfss6eQCA&bih=578&biw=1280#imgrc=Gb52RRGEEcputM");
+        }
+
+        private void linkImbracaminte_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            System.Diagnostics.Process.Start("https://www.google.com/search?q=imbracaminte+decathlon&tbm=isch&ved=2ahUKEwi04unbks_rAhWHNewKHZSdARkQ2-cCegQIABAA&oq=imbracaminte+decathlon&gs_lcp=CgNpbWcQAzoGCAAQBxAeUKi8B1jv3Adg56sIaABwAHgAgAF6iAGIC5IBBDIuMTGYAQCgAQGqAQtnd3Mtd2l6LWltZ8ABAQ&sclient=img&ei=VwNSX_TaEIfrsAeUu4bIAQ&bih=578&biw=1280#imgrc=z25HCHMHauqQiM");
+        }
+
+        private void linkBocanciDeMunte_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            System.Diagnostics.Process.Start("https://www.google.com/search?q=bocanci+de+munte+decathlon&tbm=isch&ved=2ahUKEwiowKjuus_rAhVK4qQKHYSjAsYQ2-cCegQIABAA&oq=bocanci+de+munte+decathlon&gs_lcp=CgNpbWcQAzIECAAQHjoGCAAQBxAeOgIIADoICAAQCBAHEB5Qz78rWPfbK2Cs4ytoAHAAeACAAZUBiAHLDpIBBDMuMTOYAQCgAQGqAQtnd3Mtd2l6LWltZ8ABAQ&sclient=img&ei=by1SX-jMHMrEkwWEx4qwDA&bih=578&biw=1280#imgrc=QhD_hyV7vuAnDM");
+        }
+
+        private void linkBeteDrumetie_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            System.Diagnostics.Process.Start("https://www.google.com/search?q=bete+trekking+decathlon&tbm=isch&ved=2ahUKEwjD75Trus_rAhUDP-wKHdneAHIQ2-cCegQIABAA&oq=betedecathlon&gs_lcp=CgNpbWcQARgAMgYIABAHEB4yBggAEAcQHjIGCAAQBxAeMgYIABAHEB4yBggAEAcQHjoICAAQCBAHEB5Q-BBY7RRg6SVoAHAAeACAAZsBiAGiBJIBAzAuNJgBAKABAaoBC2d3cy13aXotaW1nwAEB&sclient=img&ei=aC1SX8ObNIP-sAfZvYOQBw&bih=578&biw=1280#imgrc=I7DXRsKFHZ4q7M");
+        }
+
+    }
+}
